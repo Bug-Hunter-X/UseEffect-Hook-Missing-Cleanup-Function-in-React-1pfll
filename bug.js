3242862b18bb54f14da533387cf2201ff170949e
BugHunter-X <askzenai@gmail.com>
2024-12-10 21:10:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,16 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Incorrect: Missing return statement for cleanup function
+    fetch('https://api.example.com/data')
+      .then(response => response.json())
+      .then(data => setCount(data.count));
+  }, []);
+
+  return (
+    <div>Count: {count}</div>
+  );
+}
+```
